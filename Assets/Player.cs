c287dcdaaa288d3a43c112b624e3138e347a4929
KurryW <kwesterhuis@hotmail.com>
2023-09-18 13:51:04 +0200

    Movement

---
+++ Assets/Player.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+//using UnityEditor.Experimental.GraphView;
+
+public class Player : MonoBehaviour
+{
+	[SerializeField] WheelCollider frontRight;
+	[SerializeField] WheelCollider frontLeft;
+	[SerializeField] WheelCollider backRight;
+	[SerializeField] WheelCollider backLeft;
+
+    //private Animator anim;
+    public AudioSource footstepsSound;
+
+	public float acceleration = 500f;
+	public float breakingForce = 300f;
+	public float maxTurnAngle = 15f;
+
+	private float currentAcceleration = 0f;
+	private float currentBreakforce = 0f;
+	private float currentTurnAngle = 0f;
+
+
+    void Start()
+	{
+		//anim = gameObject.GetComponentInChildren<Animator>();
+	}
+
+	void FixedUpdate()
+	{
+
+		currentAcceleration = acceleration * Input.GetAxis("Vertical");
+
+		//if (Input.GetKey(KeyCode.W))
+		//{
+		//	//anim.SetInteger("AnimationPar", 1);
+		//	//footstepsSound.enabled = true;
+		//}
+		//else
+		//{
+		//	//anim.SetInteger("AnimationPar", 0);
+		//	//footstepsSound.enabled = false;
+		//}
+
+
+		if (Input.GetKey(KeyCode.Space))
+		
+			currentBreakforce = breakingForce;
+			//anim.SetInteger("JumpPar", 1);
+		
+		else
+		
+			currentBreakforce = 0f;
+			//anim.SetInteger("JumpPar", 0);
+		
+
+
+		frontRight.motorTorque = currentAcceleration;
+		frontLeft.motorTorque = currentAcceleration;
+
+		frontRight.brakeTorque = currentBreakforce;
+		frontLeft.motorTorque = currentBreakforce;
+		backLeft.motorTorque = currentBreakforce;
+		backRight.motorTorque = currentBreakforce;
+
+		Debug.Log(currentBreakforce);
+
+		currentTurnAngle = maxTurnAngle * Input.GetAxis("Horizontal");
+		frontLeft.steerAngle = currentTurnAngle;
+		frontRight.steerAngle = currentTurnAngle;
+
+    }
+}
