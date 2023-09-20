57a4463a1b316765e9e813d670670348c0042053
KurryW <kwesterhuis@hotmail.com>
2023-09-20 16:51:18 +0200

    new ui poison text

--- Assets/Enemyspawn.cs
+++ Assets/Enemyspawn.cs
@@ -8,10 +8,11 @@ public class Enemyspawn : MonoBehaviour
 {
     [SerializeField] private string sceneName;
 
-    public GameObject[] enemies;
+    public GameObject[] enemies = new GameObject[5];
     private int enemyIndex = 0;
 
     public GameObject SpacebarButton;
+    public GameObject PoisonUI;
 
 
     private void Start()
@@ -33,6 +34,8 @@ public class Enemyspawn : MonoBehaviour
     {
         enemies[enemyIndex].SetActive(false);
         enemyIndex++;
+        PoisonUI.SetActive(true);
+        Invoke(nameof(DisablePoisonUI), 4f);
 
         if (enemyIndex >= enemies.Length)
         {
@@ -43,6 +46,11 @@ public class Enemyspawn : MonoBehaviour
             enemies[enemyIndex].SetActive(true);
             DeactivateSpacebarButton();
         }
+    }
+
+    private void DisablePoisonUI()
+    {
+        PoisonUI.SetActive(false);
     }
     
 }
